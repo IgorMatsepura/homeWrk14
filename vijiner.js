b7e6a4697304c284eff8367f2a30b0c201e30acb
IgorMatsepura <Igor.Matsepura@gmail.com>
2018-01-22 20:25:33 +0200

    homeWrk14 v2.0

--- vijiner.js
+++ vijiner.js
@@ -1,45 +1,40 @@
-var codeWords = "zone";
-var inputLetters = "Hello words aaa";
-
+var codeWords = "zone";					//code letters
+var inputLetters = "Hello words aaa";			// input words for code
+var newCodeLetters =[];					// array new code letters
+var lengthLetters;
 
 function codeToVijiner (newCodeLetter) {
-		var newCodeLetter =[];
-		for (var i = 0, j = 0; i < inputLetters.length; i++, j++) {
+		lengthLetters = inputLetters.length;
+		for (var i = 0, j = 0; i < lengthLetters; i++, j++) {
 			if (codeWords.length <= j) {
 				j = 0;
-            }
+            		}
 			var countNumberLetter =  inputLetters.toLowerCase().charCodeAt(i);
 			var countCodeEncrypt = codeWords.toLowerCase().charCodeAt(j);
 				if (65 <= inputLetters.charCodeAt(i) && inputLetters.charCodeAt(i) <= 90){
-					inputLetters[i].toLowerCase();
-					//console.log(inputLetters[i].toLowerCase());
-                }
+					inputLetters.toLowerCase();
+                		}
 				if ( inputLetters.charCodeAt(i) !== 32) {
-					newCodeLetter =  ((countNumberLetter - 97) + (countCodeEncrypt - 97)) % 26 + 97;
+					var newCodeLetter = ((countNumberLetter - 97) + (countCodeEncrypt - 97)) % 26 + 97;
+					newCodeLetters[i] = newCodeLetter;
 					console.log(String.fromCharCode(newCodeLetter));
-					//console.log(countNumberLetter +" "+ countCodeEncrypt +" NEW LETT " + newCodeLetter +" "+ String.fromCharCode(newCodeLetter));
-					//console.log(newCodeLetter);
-                }
-        }
-		console.log(inputLetters.length);
-		console.log(codeWords.length);
-		return newCodeLetter;
+                		}
+        	}
+		return newCodeLetters;
 }
 codeToVijiner();
 
+
 function encodeVijiner () {
-		for (var i = 0, j = 0; i < newCodeLetter.length; i++, j++) {
+		for (var i = 0, j = 0; i < lengthLetters; i++, j++) {
 			if (codeWords.length <= j) {
 				j = 0;
-            }
-			console.log(newCodeLetter);
-			var countNumberLetter =  newCodeLetter.toLowerCase().charCodeAt(i);
+            		}
+			var countNumberLetter =  newCodeLetters[i];
 			var countCodeEncrypt = codeWords.toLowerCase().charCodeAt(j);
-				if ( newCodeLetter.charCodeAt(i) !== 32) {
-					var enCodeLetter =  ((countNumberLetter - 97) + (countCodeEncrypt - 97)) % 26 - 97;
-				//	console.log(countNumberLetter +" "+ countCodeEncrypt +" ENCRYPTLETT " + enCodeLetter +" "+String.fromCharCode(enCodeLetter));
-                }
-        }
-		
+					var enCodeLetters =  ((countNumberLetter - 97) + (countCodeEncrypt - 97)) % 26 + 97;
+					console.log(String.fromCharCode(enCodeLetters));
+        	}
+
 }
 encodeVijiner();	
